a539d93a461109d6e11c411a43af3360d00e927e
shrutii27 <shrutiamrutkar111@gmail.com>
2023-10-05 18:15:31 +0530

    Add project files.

---
+++ Guitar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarFinalApp.Model
+{
+    internal class Guitar
+    {
+        internal string SerialNumber { get; }
+        internal double Price { get; set; }
+
+        internal GuitarSpec Spec { get; }
+
+        public Guitar(string serialNumber, double price, GuitarSpec spec)
+        {
+            this.SerialNumber = serialNumber;
+            this.Price = price;
+            this.Spec = spec;
+        }
+    }
+}
+
+
---
+++ GuitarSpec.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarFinalApp.Model
+{
+    internal class GuitarSpec
+    {
+        public Builder builder { get; }
+        public string model { get; }
+        public Type type { get; }
+        public Wood backWood { get; }
+        public Wood topWood { get; }
+        public int numStrings { get; }
+
+        public GuitarSpec(Builder builder, string model, Type type, Wood backWood, Wood topWood, int numStrings)
+        {
+            this.builder = builder;
+            this.model = model;
+            this.type = type;
+            this.backWood = backWood;
+            this.topWood = topWood;
+            this.numStrings = numStrings;
+        }
+
+        public bool matches(GuitarSpec spec)
+        {
+           //check if matches fail
+            if (builder != spec.builder) { return false; }
+            if (!StringsMatch(model, spec.model)) { return false; }
+            if (type != spec.type) { return false; }
+            if (backWood != spec.backWood) { return false; }
+            if (topWood != spec.topWood) { return false; }
+            return true;
+        }
+
+        private bool StringsMatch(string stringA, string stringB)
+        {
+            return string.Equals(stringA, stringB, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
+
+
